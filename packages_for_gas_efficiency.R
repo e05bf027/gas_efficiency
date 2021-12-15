d7d4b719d3d659fc3154713513aa5c255cc7efa8
David M Hannon <davidmhannon@gmail.com>
2021-12-15 17:40:47 +0000

    initialise script to load packages needed for task

---
+++ packages_for_gas_efficiency.R
@@ -0,0 +1,5 @@
+# packages_for_gas_efficiency.R
+# loads packages needed for task of  creatinglarge tibbles of physiologic data
+
+library(readxl)   # to read xlsx files
+library(dplyr)   # for general data wrangling`
