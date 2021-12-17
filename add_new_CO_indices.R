c1947bdb152a776f4a82e12e9b816b4cfdbbc5ef
David M Hannon <davidmhannon@gmail.com>
2021-12-17 15:07:20 +0000

    add new CV function estimates
    utilises the method of Liljestrand & Zander

---
+++ add_new_CO_indices.R
@@ -0,0 +1,17 @@
+# add_new_CO_induces.R
+# ========================================================================
+# adds new indices of CV function based on the derivations resting on the
+# Liljestrand and Zander method for estimation of CO from arterial BP.
+#   - CO
+#   - DO2 (uses CaO2 as per add_new_gas_indices.R)
+#   - SVR (uses above CO measure)
+#
+# ========================================================================
+
+# CO (Liljestrand and Zander method)
+
+
+# DO2
+
+
+# SVR
