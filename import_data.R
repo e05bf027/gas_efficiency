614293c8641d4571a8fdc9e290d207efb7793aa9
David M Hannon <davidmhannon@gmail.com>
2021-12-20 12:32:09 +0000

    remove output from import_data.R

--- import_data.R
+++ import_data.R
@@ -17,7 +17,7 @@ untidy_tibble <-  untidy_tibble %>%
   select(Time, `Parameter Name`, Value)
 
 # next, factorize the parameter names
-untidy_tibble$`Parameter Name` <- as.factor(large_tibble$`Parameter Name`)
+untidy_tibble$`Parameter Name` <- as.factor(untidy_tibble$`Parameter Name`)
 
 # The vales in the 'cardiac rhythm;' column are awkward, and often read as
 # list_cols. To get around this, isolate the cardiac rhythm values and remove
@@ -26,7 +26,7 @@ cardiac_rhythm <- filter(untidy_tibble,
                          untidy_tibble$`Parameter Name` == 'Cardiac Rhythm')
 
 untidy_tibble <- filter(untidy_tibble,
-                        large_tibble$`Parameter Name` != 'Cardiac Rhythm')
+                        untidy_tibble$`Parameter Name` != 'Cardiac Rhythm')
 
 # Now manipulate the cardiac data independently. The initial pivot gives 
 # list_cols that are then turned to characters before the original column
@@ -40,17 +40,22 @@ cardiac_rhythm <- select(cardiac_rhythm, Time, Cardiac_rhythm)
 
 ### PIVOT THE UNTIDY TIBBLE INTO WIDE FORMAT and remove the untidy tibble
 ### replace INIT with the study ID number
-tidy_tibble_ID <- pivot_wider(untidy_tibble, 
+tidy_tibble <- pivot_wider(untidy_tibble, 
                            id_cols = Time, 
                            names_from = `Parameter Name`, 
                            values_from = Value)
 
 # Rejoin the cardiac data and arrange everything chronologically
-tidy_tibble_ID <- left_join(tidy_tibble_ID, cardiac_rhythm, by = 'Time') %>%
+tidy_tibble <- left_join(tidy_tibble, cardiac_rhythm, by = 'Time') %>%
   arrange(Time)
 
-# Save a copy of this file (IS IT GOING TO THE RIGHT DESTINATION?)
+# Save a copy of this file
 file_name <- sprintf('/Users/davidhannon/Documents/02. Medicine/Med_Programming/00. Patient DB/outputs/wide/patient_00%d.xlsx', i)
-write_xlsx(tidy_tibble_ID, file_name)
+write_xlsx(tidy_tibble, file_name)
 
-### You now have a large, wide, tibble unique to that patient
+# upload a copy to google drive
+# NB: 'drive_put' allows the file to either renew an existing file,
+# or placing a brand new file
+local_location <- file_name
+drive_location <- as_dribble('https://drive.google.com/drive/folders/1SnxmvqQnWsjr5jjSWFY5ht9FOpsi5fkg')
+drive_put(local_location, drive_location)
