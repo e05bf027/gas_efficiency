0ed49b7ee8f547fe01825e9253148b68ba2e2763
David M Hannon <davidmhannon@gmail.com>
2021-12-20 12:34:38 +0000

    alter text displayed by source_files.R

--- source_files.R
+++ source_files.R
@@ -9,9 +9,9 @@ print('The .xls files that are generated from the Metavisiion Query wizard')
 print('require pre-processing. Have you:')
 print('1. changed to extension to .xlsx from .xls?')
 print('2. removed the empty rows from the top of the excel file?')
-print('================================')
+print('====================================================================')
 print('If the file(s) has been preprocessed correctly, proceed.')
-print('================================')
+print('====================================================================')
 
 # List of raw input files
 mv_location <- "/Users/davidhannon/Documents/02. Medicine/Med_Programming/00. Patient DB/metavision_outputs"
@@ -19,6 +19,7 @@ mv_files <- list.files(mv_location, full.names = TRUE)
 view(mv_files)
 
 # choose which file you want to import and process
-print('  ')
 i <- as.numeric(readline("enter the index of the file you want to import: "))
 
+# run import script
+source('import_data.R')
