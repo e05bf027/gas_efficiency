772640ed9533bea4e7c9197d29a8f8716408eb66
David M Hannon <davidmhannon@gmail.com>
2021-12-23 18:07:07 +0000

    expanded early_explorations.R
    seems to show an improvement in PFR when prone

--- early_explorations.R
+++ early_explorations.R
@@ -2,16 +2,40 @@
 # a script to allow early exploration of the large, wide tibbles.
 # ===================================
 
-exploratory_destination <- as.character('/Users/davidhannon/Documents/02. Medicine/Med_Programming/00. Patient DB/outputs/wide/patient_003.xlsx')
-exploratory_tibble <- read_xlsx(exploratory_destination, guess_max = 1000000)
+exp_tibble <- tidy_tibble
 
-ABG_data_loc <- '/Users/davidhannon/Documents/02. Medicine/Med_Programming/MD_Metavision_wrangle/wrangled_data/03_KH/Patient_003_verified.xlsx'
+ABG_data_loc <- '/Users/davidhannon/Documents/02. Medicine/Med_Programming/MD_Metavision_wrangle/wrangled_data/04_JM/Patient_004_JM.xlsx'
 ABG_tibble <- read_xlsx(ABG_data_loc, sheet = 'ABG (transcribed to ICU comp)', guess_max = 1000000)
 
+# columns are characters, change them to dbl
+ABG_tibble[3:12] <- sapply(ABG_tibble[3:12], as.numeric)
 
-# add PFR
-exploratory_tibble$PaO2 <- as.numeric(exploratory_tibble$PaO2)
-exploratory_tibble$`Measured Fi02 (PB)` <- as.numeric(exploratory_tibble$`Measured Fi02 (PB)`)
+# prep ABG tibble for joining
+# ABG_join <- select(ABG_tibble, -'Patient Positioning')
 
-mutate(exploratory_tibble, 
-       PF_ratio = PaO2 / (exploratory_tibble$`Measured Fi02 (PB)` * 100))
+# isolate fio2
+fio2 <- select(exp_tibble, Time, `Set Fraction Inspired Oxygen (PB)`)
+
+# add fio2 to ABG_tibble
+ABG_tibble <- left_join(ABG_tibble, fio2, by = 'Time') %>%
+  arrange(Time)
+
+ABG_tibble$`Set Fraction Inspired Oxygen (PB)` <- as.double(ABG_tibble$`Set Fraction Inspired Oxygen (PB)`)
+
+# create PF_ratio
+ABG_tibble <- mutate(ABG_tibble, 
+       PF_ratio = PaO2 / (ABG_tibble$`Set Fraction Inspired Oxygen (PB)` / 100))
+
+# factorise positioning
+ABG_tibble$`Patient Positioning` <- as.factor(ABG_tibble$`Patient Positioning`)
+
+# ============================================
+# now create exploratory graphs
+
+PFR_table <- filter(ABG_tibble, PF_ratio > 0)
+
+ggplot(data = PFR_table, aes(x = Time, y = PF_ratio, colour = PaCO2)) +
+  geom_line()
+
+ggplot(data = PFR_table, aes(x = Time, y = PF_ratio, colour = `Patient Positioning`)) +
+  geom_col() # suggests improvement when prone
