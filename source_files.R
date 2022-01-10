603f7782fa375b928c890a6d789a84f10e25b097
David M Hannon <davidmhannon@gmail.com>
2022-01-10 11:34:23 +0000

    update source_files.R
    1) added step to calculate the index of the demographics file
    2) added step asking the user to designate a final index for the file
    3) removed step 2 (above) from the output script

--- source_files.R
+++ source_files.R
@@ -15,11 +15,16 @@ print('====================================================================')
 
 # List of raw input files
 mv_location <- "/Users/davidhannon/Documents/02. Medicine/Med_Programming/00. Patient DB/metavision_outputs"
-mv_files <- list.files(mv_location, full.names = TRUE)
+mv_files <- list.files(mv_location)
 view(mv_files)
 
 # choose which file you want to import and process
-i <- as.numeric(readline("enter the index of the file you want to import: "))
+i <- as.numeric(readline("enter the index of the data file you want to import: "))
+demo_index <- i-1
+
+# also decide what number you wish to designate for this patient in the final database
+j <- as.character(readline('What number do you wish to designate for this patient: '))
+file_name <- sprintf('/Users/davidhannon/Documents/02. Medicine/Med_Programming/00. Patient DB/outputs/wide/patient_0%s.xlsx', j)
 
 # run import script
 source('import_data.R')
