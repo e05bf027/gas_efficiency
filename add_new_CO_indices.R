40c455ecd47cf50ba6419a598b2113522169d9c6
David M Hannon <davidmhannon@gmail.com>
2022-01-11 15:35:33 +0000

    expand add_new_CO_indices.R
    began to add way to estimate cardiac output, but abandoned due to difficulty with confounding elements. Script 'paused'

--- add_new_CO_indices.R
+++ add_new_CO_indices.R
@@ -2,20 +2,21 @@
 # ========================================================================
 # adds new indices of CV function based on the derivations resting on the
 # Liljestrand and Zander method for estimation of CO from arterial BP.
-#   - CO
+#   - CO (Koenig 2015 paper)
 #   - DO2 (uses CaO2 as per add_new_gas_indices.R)
 #   - SVR (uses above CO measure)
 #   - CVP (using SVR = (MAP - CVP)/CO * 80)
 #
 # ========================================================================
 
-# CO (Liljestrand and Zander method)
+# tidy_co <- tidy_tibble %>% mutate(
+#   cardiac_output_lz = ((arterial_pressure_systolic - arterial_pressure_diastolic) / 
+#                       (arterial_pressure_systolic + arterial_pressure_diastolic)) *
+#                       heart_rate
+# )
 
-
-# DO2
-
-
-# SVR
-
-
-# CVP
+The problem with the above is that it leads to CO values vastly more than normal
+e.g. 40L/min. There is a correction factor, k, applied. Details of this are found
+in Sun (2005). The confounder here is that most patients have pressors running 
+as infusions (most commonly noradrenaline) AND that the reason for their presence
+in ICU can impair cardiac function (never mind PMHx).
