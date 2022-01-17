10307667f0861307c71a1ec138c7ac84b44f22c4
David M Hannon <davidmhannon@gmail.com>
2022-01-17 10:52:30 +0000

    alter file_outputs to save tibble

--- file_outputs.R
+++ file_outputs.R
@@ -8,6 +8,10 @@
 # Save a copy of the file
 write_xlsx(x = output_sheets, format_headers = T, path = file_name)
 
+# Save the tibble
+tibble_file <- 
+save(tidy_tibble, file = sprintf('/Users/davidhannon/Documents/02. Medicine/Med_Programming/00. Patient DB/dfs/patient_0%s.Rda', j))
+
 # upload a copy to google drive
 # NB: 'drive_put' allows the file to either renew an existing file,
 # or placing a brand new file
